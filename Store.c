9a517f99e78e24bbe5f4358800baf7cb52d304d5
Bassam Mahmoud Yousif <124912143+alhashiemy@users.noreply.github.com>
2023-03-11 12:39:37 +0200

    Update Store.c

--- Store.c
+++ Store.c
@@ -11,27 +11,53 @@ struct products {
     enum cat category;
     float price;
     int stock;
-    union productInfo info;
+    union productInfo pInfo;
 };
+
 union productInfo newp()
 {
     union productInfo newpr;
-    scanf("%f",newpr.floatVal);
+    int cases;
+    printf("\nenter 1 for size:\nenter 2 for weight:\n");
+    scanf("%d",&cases);
+    switch(cases)
+    {
+        case(1):
+        {
+            printf("\nEnter float value:");
+            scanf("%f",&newpr.floatVal);
+        }
+        break;
+        case(2):
+        {
+            printf("\nEnter integer value:");
+            scanf("%d",&newpr.integerVal);
+        }
+        break;
+        default:
+        {
+            printf("\nwrong answer");
+        }
+        break;
+
+    }
     return(newpr);
 };
 
 struct products entProduct()
 {
     struct products newproduct;
-    printf("product name:");
-    scanf("%s\n",&newproduct.name);
-    printf("Enter the price");
-    scanf("%f\n",&newproduct.price);
-    printf("inter the quantity:");
+    char cat_num;
+    printf("\nproduct name: ");
+    scanf("%s",&newproduct.name);
+    printf("choose category: \n0 for food\n1 for drink\n2 for toiletrien\n3 for clothing\n");
+    scanf("%d",&newproduct.category);
+    printf("\nEnter the price: ");
+    scanf("%f",&newproduct.price);
+    printf("\nEnter the quantity: ");
     scanf("%d",&newproduct.stock);
-    printf("for any additional information:");
-    newproduct.info = newp();
-
+    printf("\nadditional information of size/weight: ");
+    newproduct.pInfo = newp();
 
 
     return (newproduct);
@@ -39,8 +65,7 @@ struct products entProduct()
 int main ()
 {
     struct products product[3];
-    int i=0,j;
-    char pcat[50];
+    int i;
     printf("Enter product information\n");
     do
     {
@@ -48,10 +73,38 @@ int main ()
         i++;
     }
     while(i<3);
-    for(j=0;j<3;j++)
+    i=0;
+    do
     {
-        printf("information of product number %d :\n",j);
-        printf("neme: %s", product[i]);
+        printf("\n\ninformation of products :\n");
+        printf("neme: %s\n", product[i].name);
+        printf("category:");
+        switch(product[i].category)
+        {
+            case(food):
+                {
+                    printf("food\n");
+                }break;
+            case(drink):
+                {
+                    printf("drink\n");
+                }break;
+            case(toiletries):
+                {
+                    printf("toiletries\n");
+                }break;
+            case(clothing):
+                {
+                    printf("clothing");
+                }
+                break;
+        }
+        printf("price: %f\n",product[i].price);
+        printf("number in stock: %d\n",product[i].stock);
+        printf("additional information abour size weight: %f\n",product[i].pInfo);
+        i++;
     }
+    while(i<3);
+
     return(0);
 }
